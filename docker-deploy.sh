f20f2a9c1be49da2681676c3c4121233770c9f93
Mustafa Emre Acer <meacer@chromium.org>
2026-07-22 00:55:53 +0000

    Configure Skylight mirror and update Docker deployment files
    - Add data/skylight.yaml with monitoringprefix set to mirror1.test.mtcs.dev.
    - Update data/compose.override.yaml for CA/Mirror OID synchronization.
    - Update docker-deploy.sh to copy data/skylight.yaml to the VM.
    
    TAG=agy
    CONV=c25fb856-4258-4d28-b0ae-989864a83119

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -13,7 +13,7 @@ cd "$CACTUS_DIR"
 gcloud compute scp --recurse ./docker "$VM":~/ --zone="$ZONE" --project="$PROJECT"
 
 # Override with custom configs from cactus-deploy:
-gcloud compute scp "$DEPLOY_DIR/data/apache-docker.conf" "$DEPLOY_DIR/data/compose.override.yaml" "$VM":~/docker/ --zone="$ZONE" --project="$PROJECT"
+gcloud compute scp "$DEPLOY_DIR/data/apache-docker.conf" "$DEPLOY_DIR/data/compose.override.yaml" "$DEPLOY_DIR/data/skylight.yaml" "$VM":~/docker/ --zone="$ZONE" --project="$PROJECT"
 gcloud compute scp "$DEPLOY_DIR/data/cactus-config-docker.json" "$VM":~/docker/cactus-config.json --zone="$ZONE" --project="$PROJECT"
 gcloud compute scp "$DEPLOY_DIR/data/request-certs.sh" "$VM":~/docker/request-certs.sh --zone="$ZONE" --project="$PROJECT"
 gcloud compute ssh "$VM" --zone="$ZONE" --project="$PROJECT" -- "chmod +x ~/docker/request-certs.sh"
